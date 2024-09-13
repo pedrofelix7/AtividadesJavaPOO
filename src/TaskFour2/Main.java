5bab43aa246c3235619eddeae2de80fbcb66b8c0
Pedro Felix <pedro123felixcavalcanti@gmail.com>
2024-09-13 16:17:00 -0300

    task four att

--- src/TaskFour2/Main.java
+++ src/TaskFour2/Main.java
@@ -2,7 +2,7 @@ package TaskFour2;
 
 public class Main {
     public static void main(String[] args) {
-Pessoa minhaPessoa = new Pessoa("Pedro", "Cavalcanti", "090.114.543-98", "89.055.434", "pedrolindo123@gmail.com", "(87) 99913-0990", "Av. Coronel Antônio Japiassú, 22", "Recife", "Pernambuco", "56500-000");
+Pessoa minhaPessoa = new Pessoa("Pedro", "Cavalcanti", 19, "090.114.543-98", "89.055.434", "pedrolindo123@gmail.com", "(87) 99913-0990", "Av. Coronel Antônio Japiassú, 22", "Recife", "Pernambuco", "56500-000");
 System.out.println("O nome do cliente é: " + minhaPessoa.getNome());
 System.out.println("O sobrenome do cliente é: " + minhaPessoa.getSobrenome());
 System.out.println("O nome completo do cliente é: " + minhaPessoa.getNome() + " " + minhaPessoa.getSobrenome());
@@ -15,7 +15,7 @@ System.out.println("A cidade onde o cliente mora é: " + minhaPessoa.getCidade()
 System.out.println("O estado onde o cliente more é: " + minhaPessoa.getEstado());
 System.out.println("O cep do cliente é: " + minhaPessoa.getCep());
 
-Pessoa minhaPessoa2 = new Pessoa("Felipe", "Alves", "994.887.668-00", "10.987.412", "felipegostosinho321@gmail.com", "(81) 99130-0790", "Av José Bonifácio", "Pesqueira", "Pernambuco", "565000-001");
+Pessoa minhaPessoa2 = new Pessoa("Felipe", "Alves", 25,"994.887.668-00", "10.987.412", "felipegostosinho321@gmail.com", "(81) 99130-0790", "Av José Bonifácio", "Pesqueira", "Pernambuco", "565000-001");
         System.out.println("O nome do cliente2 é: " + minhaPessoa2.getNome());
         System.out.println("O sobrenome do cliente2 é: " + minhaPessoa2.getSobrenome());
         System.out.println("O nome completo do cliente2 é: " + minhaPessoa2.getNome() + " " + minhaPessoa.getSobrenome());
